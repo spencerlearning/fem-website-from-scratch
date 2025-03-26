e0631a1edb3e6fbc31067f5c8fae3cc8f51bf632
Brian Spencer <brian@spencerlearning.com>
2025-03-26 12:48:20 -0700

    Add hamburger menu functionality

---
+++ main.js
@@ -0,0 +1,16 @@
+const hamburgerButton = document.querySelector('[aria-controls="primary-nav"]');
+// const nav = document.querySelector(".primary-navigation");
+const nav = document.querySelector("#primary-nav");
+
+hamburgerButton.addEventListener("click", () => {
+  // check if the navigation is opened
+  const isNavOpened = hamburgerButton.getAttribute("aria-expanded");
+
+  if (isNavOpened === "false") {
+    hamburgerButton.setAttribute("aria-expanded", "true");
+  } else {
+    hamburgerButton.setAttribute("aria-expanded", "false");
+  }
+
+  // console.log(isNavOpened);
+});
